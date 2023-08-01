0b043078332d2ae70620c9f90b2ea25ae28eeca4
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-08-01 15:39:57 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+def calculate_square():
+    num = int(input("Enter an integer: "))
+    square = num ** 2
+    print("The square of", num, "is", square)
+
+
+class Calculator:
+    def __init__(self):
+        self.result = None
+
+    def square(self, num):
+        self.result = num ** 2
+
+    def print_result(self):
+        if self.result is not None:
+            print("The square is:", self.result)
+        else:
+            print("No result calculated yet.")
+
+
+def main():
+    calculator = Calculator()
+    num = int(input("Enter an integer: "))
+    calculator.square(num)
+    calculator.print_result()
+
+
+if __name__ == "__main__":
+    main()
+
+# The code provided seems to be error-free and executable. However, there are some improvements
+# that can be made to make the code more readable and maintainable.
+
+# 1. Rename the method `calculate_square` to `get_square` to better reflect its purpose.
+# 2. Add docstrings to the classes and methods to provide better documentation.
+# 3. Use f-strings for string formatting to improve readability.
+# 4. Remove the unnecessary `result` attribute from the Calculator class, as it is not being used.
+#    Square calculation can be done directly in the `square` method.
+# 5. Move the user input prompt for the integer from the `main` method to the `get_square` method,
+#    as it is more logical to get the input from the user in the method that calculates the square.
+# 6. Remove the unused calculate_square function.
+
+# Below is the improved version of the code:
+
+class Calculator:
+    """
+    Calculator class for calculating square of a number.
+    """
+
+    def square(self, num):
+        """
+        Calculates the square of the given number.
+        """
+        square = num ** 2
+        return square
+
+    def print_result(self, num, square):
+        """
+        Prints the calculated square.
+        """
+        print(f"The square of {num} is {square}")
+
+
+def main():
+    calculator = Calculator()
+    num = int(input("Enter an integer: "))
+    square = calculator.square(num)
+    calculator.print_result(num, square)
+
+
+if __name__ == "__main__":
+    main()
